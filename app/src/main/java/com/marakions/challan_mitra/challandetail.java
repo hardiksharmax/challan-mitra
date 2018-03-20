93001d7f0363fd8bcbdaff8b479e0f44d88203e3
deepakbapay <bajpay3296@gmail.com>
2018-03-20 12:40:02 +0530

    code implementation

--- app/src/main/java/com/marakions/challan_mitra/challandetail.java
+++ app/src/main/java/com/marakions/challan_mitra/challandetail.java
@@ -1,7 +1,11 @@
 package com.marakions.challan_mitra;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -14,23 +18,56 @@ import com.marakions.challan_mitra.utils.Constants;
 
 public class challandetail extends AppCompatActivity {
 
+    TextView challanNoTv, nameTv, dateTv, locationTv, categoryTv, amountTv;
+    private ChallanItem challanItem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challandetail);
+
+        challanNoTv = findViewById(R.id.challannumberview);
+        nameTv = findViewById(R.id.nameview);
+        dateTv = findViewById(R.id.challandateview);
+        locationTv = findViewById(R.id.locationview);
+        categoryTv = findViewById(R.id.Challancatview);
+        amountTv = findViewById(R.id.Challanamtview);
+
+        Button proceed = findViewById(R.id.proceed_bt);
+        proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (challanItem == null)
+                    return;
+                Intent intent = new Intent(challandetail.this, challanpayment.class);
+                intent.putExtra("challan_number", challanItem.getChallanNo());
+                intent.putExtra("amount", challanItem.getAmount());
+                startActivity(intent);
+            }
+        });
         String challanNo = getIntent().getStringExtra("challan_number");
         fetchChallanDetails(challanNo);
 
     }
 
-    private void fetchChallanDetails(String challanNo) {
+    private void fetchChallanDetails(final String challanNo) {
         DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child(Constants.CHILD_ROOT).child(challanNo);
         dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot!=null){
-                    ChallanItem challanItem = dataSnapshot.getValue(ChallanItem.class);
-                }else{
+                if (dataSnapshot != null) {
+                    challanItem = dataSnapshot.getValue(ChallanItem.class);
+                    if (challanItem==null){
+                        Toast.makeText(challandetail.this, "No challan found for this number", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    challanNoTv.setText(challanItem.getChallanNo());
+                    dateTv.setText(challanItem.getDate());
+                    nameTv.setText(challanItem.getName());
+                    categoryTv.setText(challanItem.getCategory());
+                    amountTv.setText(challanItem.getAmount());
+                    locationTv.setText(challanItem.getLocation());
+                } else {
                     Toast.makeText(challandetail.this, "No challan found for this challan number", Toast.LENGTH_SHORT).show();
                     finish();
                 }
--- app/src/main/java/com/marakions/challan_mitra/challanpayment.java
+++ app/src/main/java/com/marakions/challan_mitra/challanpayment.java
@@ -2,12 +2,39 @@ package com.marakions.challan_mitra;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class challanpayment extends AppCompatActivity {
+
+    TextView challanAmountTv,totalAmtTv,extraAmtTv;
+    CheckBox homeDelivertCb;
+    Button payBt;
+    EditText addressEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challanpayment);
+
+        challanAmountTv = findViewById(R.id.Challan_payment_amntview);
+        extraAmtTv = findViewById(R.id.deliveryamnt);
+        homeDelivertCb =findViewById(R.id.deliverycheck);
+        addressEt =findViewById(R.id.Address);
+        payBt =findViewById(R.id.Payamount);
+
+        String amount = getIntent().getStringExtra("amount");
+        String challanNo = getIntent().getStringExtra("challan_number");
+
+        challanAmountTv.setText(amount);
+        payBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
