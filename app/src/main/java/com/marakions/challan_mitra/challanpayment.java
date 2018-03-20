8b9b076ad43d952182c87f651b27c8af96806e08
deepakbapay <bajpay3296@gmail.com>
2018-03-20 19:18:36 +0530

    code implementation3

--- app/src/main/java/com/marakions/challan_mitra/challanpayment.java
+++ app/src/main/java/com/marakions/challan_mitra/challanpayment.java
@@ -1,24 +1,27 @@
 package com.marakions.challan_mitra;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.wang.avi.AVLoadingIndicatorView;
 
-public class challanpayment extends AppCompatActivity {
+public class challanpayment extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
-    TextView challanAmountTv,totalAmtTv,extraAmtTv;
+    TextView challanAmountTv, totalAmtTv, extraAmtTv;
     CheckBox homeDelivertCb;
     Button payBt;
     EditText addressEt;
-    private Thread welcomeThread;
     AVLoadingIndicatorView avi;
+    String amount;
+    String challanNo;
+    private Thread welcomeThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +30,18 @@ public class challanpayment extends AppCompatActivity {
 
         challanAmountTv = findViewById(R.id.Challan_payment_amntview);
         extraAmtTv = findViewById(R.id.deliveryamnt);
-        homeDelivertCb =findViewById(R.id.deliverycheck);
-        addressEt =findViewById(R.id.Address);
-        payBt =findViewById(R.id.Payamount);
+        homeDelivertCb = findViewById(R.id.deliverycheck);
+        addressEt = findViewById(R.id.Address);
+        payBt = findViewById(R.id.Payamount);
         avi = findViewById(R.id.avi_challan_payment);
+        totalAmtTv = findViewById(R.id.total_amt);
+        extraAmtTv.setEnabled(false);
 
-        final String amount = getIntent().getStringExtra("amount");
-        final String challanNo = getIntent().getStringExtra("challan_number");
+        homeDelivertCb.setOnCheckedChangeListener(this);
+        amount = getIntent().getStringExtra("amount");
+        challanNo = getIntent().getStringExtra("challan_number");
+
+        totalAmtTv.setText(amount);
 
         challanAmountTv.setText(amount);
         payBt.setOnClickListener(new View.OnClickListener() {
@@ -55,14 +63,30 @@ public class challanpayment extends AppCompatActivity {
                 } catch (Exception e) {
 
                 } finally {
-                    Intent intent = new Intent(challanpayment.this,paymentgateway.class);
-                    intent.putExtra("challan_number",challanNo);
-                    intent.putExtra("amount",amount);
+                    Intent intent = new Intent(challanpayment.this, paymentgateway.class);
+                    intent.putExtra("challan_number", challanNo);
+                    intent.putExtra("amount", amount);
                     startActivity(intent);
                     finish();
                 }
             }
         };
 
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if (b) {
+            addressEt.setVisibility(View.VISIBLE);
+            extraAmtTv.setEnabled(true);
+            int total = Integer.parseInt(amount) + Integer.parseInt(extraAmtTv.getText().toString().trim());
+            totalAmtTv.setText(String.valueOf(total));
+
+        } else {
+            addressEt.setVisibility(View.GONE);
+            extraAmtTv.setEnabled(false);
+            totalAmtTv.setText(amount);
+
+        }
     }
 }
