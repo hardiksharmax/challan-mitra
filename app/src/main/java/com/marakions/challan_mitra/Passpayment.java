59a2711ad4749370e34a95b6fea227c0d8e28d2b
deepakbapay <bajpay3296@gmail.com>
2018-03-21 01:04:10 +0530

    funalsdf

--- app/src/main/java/com/marakions/challan_mitra/Passpayment.java
+++ app/src/main/java/com/marakions/challan_mitra/Passpayment.java
@@ -1,13 +1,28 @@
 package com.marakions.challan_mitra;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Passpayment extends AppCompatActivity {
+
+    Button payBt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passpayment);
+
+        payBt = findViewById(R.id.pay_bt);
+
+        payBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Passpayment.this,paymentgateway.class);
+                startActivity(intent);
+            }
+        });
     }
 }
