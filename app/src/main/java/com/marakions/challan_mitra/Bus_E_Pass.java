b03e6f919d42e6ee03e3b5e6452697d2171c0f31
Hardik <hardiksharmax@gmail.com>
2018-03-20 19:16:25 +0530

    Busspass

---
+++ app/src/main/java/com/marakions/challan_mitra/Bus_E_Pass.java
@@ -0,0 +1,13 @@
+package com.marakions.challan_mitra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bus_E_Pass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus__e__pass);
+    }
+}
